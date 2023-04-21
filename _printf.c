9ac89b5d95fd683ec384d2c8ea57115fc0422f2d
“mavine4512” <mavineoyugi@gmail.com>
2023-04-21 13:22:52 -0700

    fixing get size

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, beff_ind =0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -39,7 +39,7 @@ int _printf(const char *format, ...)
 			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			print_chars += printed;
+			printed_chars += printed;
 		}
 	}
 
--- main.h
+++ main.h
@@ -6,9 +6,11 @@
 #include <stdio.h>
 
 #define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
 
 /*Prototype functions*/
 int _printf(const char *format, ...);
+int  handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /*Function to handle other specifiers */
 int get_flags(const char *format, int *i);
@@ -16,7 +18,26 @@ int get_size(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 
+/*Sizes*/
+#define S_SHORT 1
+#define S_LONG 2
+
+/*Flags*/
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 /*Functions to print charactors*/
-int print_char(va_list types, char buffer[], int flag, int width, int precision int size);
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+/*UTILS*/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
